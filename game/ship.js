c430ddf15915cfda6a02d1708ec5d5314515ce62
Ansal <ansal@bssatech.com>
2014-06-15 14:48:48 +0530

    moved ship into a seperate object

---
+++ game/ship.js
@@ -0,0 +1,145 @@
+// code for ship
+
+var SpaceWars = SpaceWars || {};
+
+(function(){
+
+  SpaceWars.PlayerShip = {
+
+    loadAssets: function(stage) {
+      stage.game.load.image('playerShip', 
+        'assets/images/ships/playerShip.png');
+      stage.game.load.image('laser',
+        'assets/images/lasers/laserBlue.png');
+    },
+
+    createShip: function(stage) {
+      
+      // spaceship motion constants
+      stage.shipConstants = {};
+      stage.shipConstants.ROTATION_SPEED = 240; // degrees per second
+      stage.shipConstants.ACCELERATION = 300; // pixels per second
+      stage.shipConstants.MAX_SPEED = 400; // pixels per second
+      stage.shipConstants.DRAG = 100; // pixels per second
+
+      // add the ship to the stage
+      stage.ship = stage.game.add.sprite(
+        stage.game.width/2, 
+        stage.game.height/2, 'playerShip'
+      );
+      stage.ship.anchor.setTo(0.5, 0.5);
+      stage.ship.angle = -90;
+
+      // enable physics on the ship
+      stage.game.physics.enable(stage.ship, Phaser.Physics.ARCADE);
+      stage.ship.body.maxVelocity.setTo(stage.shipConstants.MAX_SPEED, stage.shipConstants.MAX_SPEED);
+      stage.ship.body.drag.setTo(stage.shipConstants.DRAG, stage.shipConstants.DRAG);
+
+      // laser motion constants
+      stage.laserConstants = {};
+      stage.laserConstants.SHOT_DELAY = 100; // in milliseconds (10 bullets per second)
+      stage.laserConstants.LASER_SPEED = 500;
+      stage.laserConstants.NUMBER_OF_LASERS = 20;
+      // create a group of lasers
+      stage.laserPool = stage.game.add.group();
+      for(var i = 0; i < stage.laserConstants.NUMBER_OF_LASERS; i += 1) {
+        var laser = stage.game.add.sprite(0, 0, 'laser');
+        stage.laserPool.add(laser);
+        laser.anchor.setTo(0.5, 0.5);
+        stage.game.physics.enable(laser, Phaser.Physics.ARCADE);
+        laser.kill();
+      }
+
+    },
+
+    controlShip: function(stage) {
+
+      // ship movements
+      if(stage.ship.x > stage.game.width) {
+        stage.ship.x = 0
+      };
+      if(stage.ship.x < 0) {
+        stage.ship.x = stage.game.width;
+      }
+      if(stage.ship.y > stage.game.height) {
+        stage.ship.y = 0;
+      }
+      if(stage.ship.y < 0) {
+        stage.ship.y = stage.game.height;
+      }
+      if(this.isLeftKeyDown(stage)) {
+        stage.ship.body.angularVelocity = -stage.shipConstants.ROTATION_SPEED;
+      } else if (this.isRightKeyDown(stage)) {
+        stage.ship.body.angularVelocity = stage.shipConstants.ROTATION_SPEED;
+      } else {
+        stage.ship.body.angularVelocity = 0;
+      }
+      if(this.isUpKeyDown(stage)) {
+        stage.ship.body.acceleration.x = Math.cos(stage.ship.rotation) 
+          * stage.shipConstants.ACCELERATION;
+        stage.ship.body.acceleration.y = Math.sin(stage.ship.rotation)
+          * stage.shipConstants.ACCELERATION;
+      } else {
+        stage.ship.body.acceleration.setTo(0, 0);
+      }
+
+      if(this.isSpaceKeyDown(stage)) {
+        this.shootLaser(stage);
+      }
+
+    },
+
+    shootLaser: function(stage) {
+
+      if(this.lastLaserShotAt === undefined) {
+        this.lastLaserShotAt = 0;
+      }
+
+      if(stage.game.time.now - this.lastLaserShotAt < stage.laserConstants.SHOT_DELAY) {
+        return;
+      }
+      this.lastLaserShotAt = stage.game.time.now;
+
+      var laser = stage.laserPool.getFirstDead();
+      if(laser === null || laser === undefined) {
+        return;
+      }
+      laser.revive();
+
+      laser.checkWorldBounds = true;
+      laser.outOfBoundsKill = true;
+
+      laser.reset(stage.ship.x, stage.ship.y);
+      laser.rotation = stage.ship.rotation;
+
+      laser.body.velocity.x = Math.cos(laser.rotation) 
+        * stage.laserConstants.LASER_SPEED;
+      laser.body.velocity.y = Math.sin(laser.rotation)
+        * stage.laserConstants.LASER_SPEED;
+
+    },
+
+    // keyboard controls
+    isLeftKeyDown: function(stage) {
+      return stage.input.keyboard.isDown(Phaser.Keyboard.LEFT);
+    },
+
+    isRightKeyDown: function(stage) {
+      return stage.input.keyboard.isDown(Phaser.Keyboard.RIGHT);
+    },
+
+    isUpKeyDown: function(stage) {
+      return stage.input.keyboard.isDown(Phaser.Keyboard.UP);
+    },
+
+    isSpaceKeyDown: function(stage) {
+      return stage.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR);
+    },
+
+    isDownKeyDown: function(stage) {
+      return stage.input.keyboard.isDown(Phaser.Keyboard.DOWN);
+    },
+
+  };
+
+})();
--- game/stage-1.js
+++ game/stage-1.js
@@ -13,10 +13,7 @@ var SpaceWars = SpaceWars || {};
   // load assets
   S1.prototype.preload = function() {
 
-    this.game.load.image('playerShip', 
-      'assets/images/ships/playerShip.png');
-    this.game.load.image('laser', 'assets/images/lasers/laserBlue.png');
-
+    SpaceWars.PlayerShip.loadAssets(this);
 
   };
 
@@ -24,122 +21,14 @@ var SpaceWars = SpaceWars || {};
 
     this.game.stage.backgroundColor = 0x333333;
 
-    // spaceship motion constants
-    this.ROTATION_SPEED = 180; // degrees per second
-    this.ACCELERATION = 200; // pixels per second
-    this.MAX_SPEED = 250; // pixels per second
-    this.DRAG = 50; // pixels per second
-
-    // add the ship to the stage
-    this.ship = this.game.add.sprite(this.game.width/2, 
-      this.game.height/2, 'playerShip');
-    this.ship.anchor.setTo(0.5, 0.5);
-    this.ship.angle = -90;
-
-    // enable physics on the ship
-    this.game.physics.enable(this.ship, Phaser.Physics.ARCADE);
-    this.ship.body.maxVelocity.setTo(this.MAX_SPEED, this.MAX_SPEED);
-    this.ship.body.drag.setTo(this.DRAG, this.DRAG);
-
-    // laser motion constants
-    this.SHOT_DELAY = 100; // in milliseconds (10 bullets per second)
-    this.LASER_SPEED = 500;
-    this.NUMBER_OF_LASERS = 20;
-    // create a group of lasers
-    this.laserPool = this.game.add.group();
-    for(var i = 0; i < this.NUMBER_OF_LASERS; i += 1) {
-      var laser = this.game.add.sprite(0, 0, 'laser');
-      this.laserPool.add(laser);
-      laser.anchor.setTo(0.5, 0.5);
-      this.game.physics.enable(laser, Phaser.Physics.ARCADE);
-      laser.kill();
-    }
-
-
-  };
-
-  S1.prototype.shootLaser = function() {
-
-    if(this.lastLaserShotAt === undefined) {
-      this.lastLaserShotAt = 0;
-    }
-
-    if(this.game.time.now - this.lastLaserShotAt < this.SHOT_DELAY) {
-      return;
-    }
-    this.lastLaserShotAt = this.game.time.now;
-
-    var laser = this.laserPool.getFirstDead();
-    if(laser === null || laser === undefined) {
-      return;
-    }
-    laser.revive();
-
-    laser.checkWorldBounds = true;
-    laser.outOfBoundsKill = true;
-
-    laser.reset(this.ship.x, this.ship.y);
-    laser.rotation = this.ship.rotation;
-
-    laser.body.velocity.x = Math.cos(laser.rotation) 
-      * this.LASER_SPEED;
-    laser.body.velocity.y = Math.sin(laser.rotation)
-      * this.LASER_SPEED;
+    SpaceWars.PlayerShip.createShip(this);
 
   };
 
   S1.prototype.update = function() {
 
-    // ship movements
-    if(this.ship.x > this.game.width) {
-      this.ship.x = 0
-    };
-    if(this.ship.x < 0) {
-      this.ship.x = this.game.width;
-    }
-    if(this.ship.y > this.game.height) {
-      this.ship.y = 0;
-    }
-    if(this.ship.y < 0) {
-      this.ship.y = this.game.height;
-    }
-    if(this.isLeftKeyDown()) {
-      this.ship.body.angularVelocity = -this.ROTATION_SPEED;
-    } else if (this.isRightKeyDown()) {
-      this.ship.body.angularVelocity = this.ROTATION_SPEED;
-    } else {
-      this.ship.body.angularVelocity = 0;
-    }
-    if(this.isUpKeyDown()) {
-      this.ship.body.acceleration.x = Math.cos(this.ship.rotation) 
-        * this.ACCELERATION;
-      this.ship.body.acceleration.y = Math.sin(this.ship.rotation)
-        * this.ACCELERATION;
-    } else {
-      this.ship.body.acceleration.setTo(0, 0);
-    }
+    SpaceWars.PlayerShip.controlShip(this);
 
-    if(this.isSpaceKeyDown()) {
-      this.shootLaser();
-    }
-
-  };
-
-  // keyboard controls
-  S1.prototype.isLeftKeyDown = function() {
-    return this.input.keyboard.isDown(Phaser.Keyboard.LEFT);
-  };
-
-  S1.prototype.isRightKeyDown = function() {
-    return this.input.keyboard.isDown(Phaser.Keyboard.RIGHT);
-  };
-
-  S1.prototype.isUpKeyDown = function() {
-    return this.input.keyboard.isDown(Phaser.Keyboard.UP);
-  };
-
-  S1.prototype.isSpaceKeyDown = function() {
-    return this.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR);
   };
 
 })();
